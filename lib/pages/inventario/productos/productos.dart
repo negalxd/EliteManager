d736bc39ea66be91ac3601d0a6029dd96e0738b4
Benjamin <103909682+Benjaxdddd@users.noreply.github.com>
2023-04-08 17:25:41 -0400

    cambios rutas
    rutas

--- lib/pages/inventario/productos/productos.dart
+++ lib/pages/inventario/productos/productos.dart
@@ -1,1 +1,3 @@
 export 'package:elite_manager/pages/inventario/productos/productos_home.dart';
+export 'package:elite_manager/pages/inventario/productos/Categorias/categorias.dart';
+export 'package:elite_manager/pages/inventario/productos/Productos_page/productospag.dart';
