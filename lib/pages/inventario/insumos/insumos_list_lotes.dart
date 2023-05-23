0a47517c1af1f26ea35b217a9ac12e4502265aae
Benjamin <103909682+Benjaxdddd@users.noreply.github.com>
2023-05-23 02:44:19 -0400

    insumos listos tanto listar como borrar falta la confirmacion de fechas
    xd

--- lib/pages/inventario/insumos/insumos_list_lotes.dart
+++ lib/pages/inventario/insumos/insumos_list_lotes.dart
@@ -19,111 +19,159 @@ class _SuppliesLoteScreenState extends State<SuppliesLoteScreen> {
   int insumoId = 0;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _fetchProductoName();
+  void initState() {
+    super.initState();
     _fetchLotesInsumos();
   }
 
-  Future<void> _fetchProductoName() async {
-  final arguments = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
-  if (arguments != null && arguments.containsKey('insumoId')) {
-    setState(() {
-      insumoId = int.parse(arguments['insumoId'].toString());
-    });
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _fetchInsumoId();
+    _fetchLotesInsumos();
   }
-}
+
+  Future<void> _fetchInsumoId() async {
+    final arguments = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+    if (arguments != null && arguments.containsKey('id_insumo')) {
+      setState(() {
+        insumoId = int.parse(arguments['id_insumo'].toString());
+      });
+    }
+  }
 
   Future<void> _fetchLotesInsumos() async {
-    final response = await http.get(Uri.parse('http://${Configuracion.apiurl}/lotes-insumos/lotes/$insumoId/'));
-    final List<dynamic> responseData = json.decode(utf8.decode(response.bodyBytes));
-    setState(() {
-      _lotesInsumos = responseData.cast<Map<String, dynamic>>();
-      _filteredLotesInsumos = _lotesInsumos.toList();
-    });
+    final url = 'http://${Configuracion.apiurl}/Api/lotes-insumos/lotes/$insumoId/';
+    final response = await http.get(Uri.parse(url));
+    print('Response: ${response.body}');
+    if (response.statusCode == 200) {
+      final List<dynamic> responseData = json.decode(utf8.decode(response.bodyBytes));
+      setState(() {
+        _lotesInsumos = responseData.cast<Map<String, dynamic>>();
+        _filteredLotesInsumos = _lotesInsumos.toList();
+      });
+    } else {
+      // Manejar el error de la solicitud HTTP aquí
+    }
   }
 
   void _filterLotesInsumos(String query) {
     setState(() {
       _filteredLotesInsumos = _lotesInsumos
           .where((loteInsumo) =>
-              loteInsumo['n_lote'].toLowerCase().contains(query.toLowerCase()) ||
-              loteInsumo['stock'].toString().contains(query))
+              loteInsumo['n_lote'].toLowerCase().contains(query.toLowerCase()))
           .toList();
     });
   }
+
+
+  void _confirmDelete(Map<String, dynamic> loteInsumo) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Eliminar insumo"),
+          content: Text("¿Está seguro que desea eliminar el insumo ${loteInsumo['n_lote']}?, esta acción no se puede deshacer."),
+          actions: [
+            TextButton(
+              child: const Text("Cancelar"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text(
+                "Eliminar",
+                style: TextStyle(color: Colors.red),
+              ),
+              onPressed: () {
+                _deleteInsumo(loteInsumo);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteInsumo(Map<String, dynamic> loteInsumo) async {
+  final response = await http.delete(Uri.parse('http://${Configuracion.apiurl}/Api/lotes-insumos/${loteInsumo['id']}/'));
+  if (response.statusCode == 200) {
+    setState(() {
+      _lotesInsumos.remove(loteInsumo);
+      _filteredLotesInsumos = _lotesInsumos;
+    });
+  //mostrar Snack de confirmacion de eliminacion
+  // ignore: use_build_context_synchronously
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('El insumo ${loteInsumo['n_lote']} ha sido eliminado correctamente'),
+    ));
+  } else {
+    print('Error al eliminar el insumo');
+  }
+}
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lotes Insumos'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.person),
-            onPressed: () {
-              Navigator.pushNamed(context, 'profile');
-            },
-          ),
-        ],
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(bottom: 10.0),
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Row(
+            body: Column(
               children: [
-                Expanded(
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: TextField(
                     controller: _searchController,
                     onChanged: _filterLotesInsumos,
-                    decoration: const InputDecoration(
-                      hintText: 'Buscar',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(),
+                    decoration: InputDecoration(
+                      hintText: 'Buscar lotes',
+                      prefixIcon: const Icon(Icons.search),
                     ),
                   ),
                 ),
-                IconButton(
-                  icon: const Icon(Icons.clear),
-                  onPressed: () {
-                    setState(() {
-                      _searchController.text = '';
-                      _filteredLotesInsumos = _lotesInsumos;
-                    });
-                  },
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _filteredLotesInsumos.length,
-              itemBuilder: (context, index) {
-                final loteInsumo = _filteredLotesInsumos[index];
-                return InkWell(
-                  onTap: () {
-                    // Acción a realizar al hacer clic en un loteInsumo
-                  },
-                  child: Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-                    child: ListTile(
-                      leading: Container(
-                        width: 50.0,
-                        child: Image.asset('assets/vianesas.png'),
-                      ),
-                      title: Text(loteInsumo['n_lote']),
-                      subtitle: Text('Stock: ${loteInsumo['stock']}'),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _filteredLotesInsumos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final loteInsumo = _filteredLotesInsumos[index];
+                return Container(
+                  margin: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.shade300,
+                      width: 1.0,
                     ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: ListTile(
+                  leading: CircleAvatar(
+                    child: FadeInImage(
+                      placeholder: const AssetImage('assets/Loading_icon.gif'),
+                      image: AssetImage('assets/gestionProd.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                    title: Text(loteInsumo['n_lote']),
+                    subtitle: Text('${loteInsumo['stock']}'),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: const Color.fromARGB(255, 182, 30, 19),
+                      onPressed: () {
+                        _confirmDelete(loteInsumo);
+                      },
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(context, 'error');
+                    },
                   ),
                 );
-              },
+                },
+              ),
             ),
-          ),
         ],
       ),
     );
